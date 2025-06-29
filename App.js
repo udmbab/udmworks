f28c00c55dda8b424be96f255e492a8527d462cf
udmbab <babu@udmedia.in>
2025-06-29 21:06:02 +0530

    https://github.com/YOUR_USERNAME/udmworks.git
    "Initial commit"

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-2">UDMworks Dashboard</h1>
+      <p className="text-sm text-gray-500">Welcome to the project management platform for ad agencies.</p>
+    </div>
+  );
+}
+
+export default App;
